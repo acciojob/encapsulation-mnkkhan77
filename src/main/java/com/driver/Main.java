19a18396b3d0f1c9d5e8a0f69138ea87a73434e5
mnkkhan <mnkkhan77@gmail.com>
2025-01-14 22:56:02 +0530

    - Assignment solved encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Name");
+        // 'name' has private access in 'com.driver.RWOnly
+        // System.out.println(rwOnly.name);
+        System.out.println(rwOnly.getName());
+    }
 }
